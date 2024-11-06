664f1b892bcf748e0b20c50321684e4bea34e532
Justin Buist <justin.buist@gmail.com>
2024-11-06 14:55:10 -0500

    Clean up split and retrieval

--- main.py
+++ main.py
@@ -235,10 +235,9 @@ class MainWindow(wx.Frame):
             curr_mins = datetime.now().minute
             # Use wx.CallAfter to update the StaticText in the main thread
             wx.CallAfter(self.clock_display.SetLabel, current_time)
-            if _app_settings['auto_out_time'] is not None:
-                out_hour, out_min = _app_settings['auto_out_time'].split(':')
-                out_hour = int(out_hour)
-                out_min = int(out_min)
+            auto_out_time = _app_settings.get('auto_out_time', None)
+            if auto_out_time is not None:
+                out_hour, out_min = map(int, auto_out_time.split(':'))
                 if out_hour > curr_hour and out_min > curr_mins:
                     self.punch_all_out(None)
 
